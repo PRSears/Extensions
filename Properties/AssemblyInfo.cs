1be5deb78363ffd4c25dce20c9e23fe72a7c2406
Patrick Sears <github@patricksears.ca>
2016-03-20 18:02:34 -0600

    Added RealToFraction converter

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -10,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Patrick Sears")]
 [assembly: AssemblyProduct("Extender")]
-[assembly: AssemblyCopyright("Copyright © Patrick Sears 2014")]
+[assembly: AssemblyCopyright("Copyright © Patrick Sears 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
